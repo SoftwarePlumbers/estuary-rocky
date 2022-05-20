e5b77c1c6626fe2f028d2d157421a95720087ed9
Jonathan Essex <Jonathan.Essex@softwareplumbers.com>
2022-05-19 21:29:25 -0400

    Added docker-compose.yml and some more initialization

--- usr/local/bin/entrypoint.sh
+++ usr/local/bin/entrypoint.sh
@@ -2,6 +2,9 @@
 if [ "$1" == "shuttle" ]; then
     source /etc/estuary-shuttle/log.env
     /usr/local/bin/estuary-shuttle --config=/etc/estuary-shuttle/config.json
+elif [ "$1" == "init" ]; then
+    source /etc/estuary/log.env
+    /usr/local/bin/estuary --config=/etc/estuary/config.json setup    
 else
     source /etc/estuary/log.env
     /usr/local/bin/estuary --config=/etc/estuary/config.json
